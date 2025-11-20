334d6b356a7e8bb49e719cfde19234b1078aacca
ilyadushko <id9104875003@yandex.ru>
2025-11-20 15:41:38 +0300

    feat: complete sprint 5, add navigation and final resources

---
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -0,0 +1,10 @@
+package com.example.playlistmaker
+
+import android.app.Activity
+import android.os.Bundle
+
+class SearchActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
