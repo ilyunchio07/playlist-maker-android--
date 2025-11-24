bd1bbe4b72191d758bc5a3c6fa602a67260c75ee
ilyadushko <id9104875003@yandex.ru>
2025-11-24 12:54:33 +0300

    feat: finish sprint 8, implement search logic with ViewModel and MVVM

--- app/src/main/java/com/example/playlistmaker/creator/Storage.kt
+++ app/src/main/java/com/example/playlistmaker/creator/Storage.kt
@@ -3,7 +3,28 @@ package com.example.playlistmaker.creator
 import com.example.playlistmaker.data.dto.TrackDto
 
 class Storage {
+
+    private val trackList = listOf(
+        TrackDto("Группа крови", "Кино", 265000, "url1"),
+        TrackDto("Танцевать", "Ёлка", 222000, "url2"),
+        TrackDto("Черный бумер", "Серега", 241000, "url3"),
+        TrackDto("Звезда по имени Солнце", "Кино", 228000, "url4"),
+        TrackDto("Вечно молодой", "Смысловые Галлюцинации", 259000, "url5"),
+        TrackDto("Полковнику никто не пишет", "Би-2", 274000, "url6"),
+        TrackDto("Прекрасное далёко", "Гости из Будущего", 231000, "url7"),
+        TrackDto("Яндекс", "L'One", 190000, "url8"),
+        TrackDto("Восьмиклассница", "Кино", 233000, "url9"),
+        TrackDto("Моя бабушка курит трубку", "Гарик Сукачёв", 276000, "url10")
+    )
+
     fun search(expression: String): List<TrackDto> {
-        return emptyList()
+        if (expression.isEmpty()) return emptyList()
+
+        val normalizedQuery = expression.lowercase()
+
+        return trackList.filter {
+            it.trackName.lowercase().contains(normalizedQuery) ||
+                    it.artistName.lowercase().contains(normalizedQuery)
+        }
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/ui/view_model/SearchViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.playlistmaker.ui.view_model
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.playlistmaker.creator.Creator
+import com.example.playlistmaker.domain.api.TracksRepository
+import com.example.playlistmaker.ui.state.SearchState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class SearchViewModel(
+    private val tracksRepository: TracksRepository
+) : ViewModel() {
+
+    private val _searchScreenState = MutableStateFlow<SearchState>(SearchState.Initial)
+    val searchScreenState: StateFlow<SearchState> = _searchScreenState.asStateFlow()
+
+    fun search(query: String) {
+        if (query.isEmpty()) return
+
+        _searchScreenState.value = SearchState.Loading
+
+        try {
+            val tracks = tracksRepository.searchTracks(query)
+
+            if (tracks.isNotEmpty()) {
+                _searchScreenState.value = SearchState.Content(tracks)
+            } else {
+                _searchScreenState.value = SearchState.Empty
+            }
+        } catch (e: Exception) {
+            _searchScreenState.value = SearchState.Error("Ошибка сети")
+        }
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                SearchViewModel(
+                    tracksRepository = Creator.provideTracksRepository()
+                )
+            }
+        }
+    }
+}
